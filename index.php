4adab08608a0639509029cbfd088fba7b0a7e98f
xJA10x <>
2020-07-24 11:16:09 -0400

    Clean the code

--- index.php
+++ index.php
@@ -6,85 +6,77 @@
         <?php include("include/header.html"); ?>
       </div>
     </header>
-
-
     <div id="wrapper">
-    <section>
-      <article>
-        <h2>>_Checkout our portfolio websites below</h2>
-
-        <!-- Bootstrap website cards -->
-
+      <section>
+        <article>
+          <h2>>_Checkout our portfolio websites below</h2>
+          <!-- Bootstrap website cards -->
+          <div class="card-deck">
+              <div class="card">
+                <img class="card-img-top" src="pictures/headphones.png" alt="Card image cap">
+                <div class="card-body">
+                  <h5 class="card-title">Ecommerce:</h5>
+                  <p class="card-text"> make your ecommerce website look fantastic</p>
+                </div>
+                <div class="card-footer">
+                  <small class="text-muted">Last updated 3 mins ago</small>
+                </div>
+              </div>
+              <div class="card">
+                <img class="card-img-top" src="pictures/bestcity.png" alt="Card image cap">
+                 <div class="card-body">
+                  <h5 class="card-title">One,Two, Three Website:</h5>
+                  <p class="card-text">Need something as one two three; we can help you</p>
+                </div>
+               <div class="card-footer">
+                 <small class="text-muted">Last updated 3 mins ago</small>
+               </div>
+              </div>
+            <div class="card">
+              <img class="card-img-top" src="pictures/cyclepathology.png" alt="Card image cap">
+              <div class="card-body">
+                <h5 class="card-title">Blog:</h5>
+                <p class="card-text">Need to express your passion then let us help you.</p>
+              </div>
+              <div class="button">
+                <small class="text-muted">Last updated 3 mins ago</small>
+              </div>
+            </div>
+          </div>
+        <!-- Adds break line -->
+        <br />
         <div class="card-deck">
-  <div class="card">
-    <img class="card-img-top" src="pictures/headphones.png" alt="Card image cap">
-    <div class="card-body">
-      <h5 class="card-title">Ecommerce:</h5>
-      <p class="card-text"> make your ecommerce website look fantastic</p>
-    </div>
-    <div class="card-footer">
-      <small class="text-muted">Last updated 3 mins ago</small>
-    </div>
-  </div>
-  <div class="card">
-    <img class="card-img-top" src="pictures/bestcity.png" alt="Card image cap">
-    <div class="card-body">
-      <h5 class="card-title">One,Two, Three Website:</h5>
-      <p class="card-text">Need something as one two three; we can help you</p>
-    </div>
-    <div class="card-footer">
-      <small class="text-muted">Last updated 3 mins ago</small>
-    </div>
-  </div>
-  <div class="card">
-    <img class="card-img-top" src="pictures/cyclepathology.png" alt="Card image cap">
-    <div class="card-body">
-      <h5 class="card-title">Blog:</h5>
-      <p class="card-text">Need to express your passion then let us help you.</p>
-    </div>
-    <div class="button">
-      <small class="text-muted">Last updated 3 mins ago</small>
-    </div>
-  </div>
-</div>
-
-<!-- Adds break line -->
-<br />
-
-<div class="card-deck">
-  <div class="card">
-    <img class="card-img-top" src="pictures/check.png" alt="Card image cap">
-    <div class="card-body">
-      <h5 class="card-title">Web App:</h5>
-      <p class="card-text"> Need a full stack application then let us help you</p>
-    </div>
-    <div class="card-footer">
-      <small class="text-muted">Last updated 3 mins ago</small>
-    </div>
-  </div>
-  <div class="card">
-    <img class="card-img-top" src="pictures/LakeTahoe.png" alt="Card image cap">
-    <div class="card-body">
-      <h5 class="card-title">Landing page:</h5>
-      <p class="card-text">Need a landing page; we can help you</p>
-    </div>
-    <div class="card-footer">
-      <small class="text-muted">Last updated 3 mins ago</small>
-    </div>
-  </div>
-  <div class="card">
-    <img class="card-img-top" src="pictures/Profeesional.png" alt="Card image cap">
-    <div class="card-body">
-      <h5 class="card-title">Blog</h5>
-      <p class="card-text">Need to express your passion then let us help you.</p>
-    </div>
-    <div class="button">
-      <small class="text-muted">Last updated 3 mins ago</small>
-    </div>
-  </div>
-</div>
-
- 
+          <div class="card">
+            <img class="card-img-top" src="pictures/check.png" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">Full Stack:</h5>
+              <p class="card-text">Need a full stack web application, we can make it happen</p>
+            </div>
+            <div class="card-footer">
+              <small class="text-muted">Last updated 3 mins ago</small>
+            </div>
+          </div>
+          <div class="card">
+            <img class="card-img-top" src="pictures/LakeTahoe.png" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">Landing page:</h5>
+              <p class="card-text">Need a landing page; we can help you</p>
+            </div>
+            <div class="card-footer">
+              <small class="text-muted">Last updated 3 mins ago</small>
+            </div>
+          </div>
+          <div class="card">
+            <img class="card-img-top" src="pictures/Profeesional.png" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">Blog:</h5>
+              <p class="card-text">Need to express your passion then let us help you.</p>
+            </div>
+            <div class="button">
+              <small class="text-muted">Last updated 3 mins ago</small>              </div>
+           </div>
+          </div>
+      </article>
     </section>
   </div>
   <footer class="main-footer">
