a40c5226c1038f5a6709fcb76275d19ed820937c
mlandy09 <mc_louigene@yahoo.com>
2020-08-18 17:53:47 -0400

    08/18/2020

--- index.php
+++ index.php
@@ -6,89 +6,99 @@
       <?php include("include/header.html"); ?>
     </div>
   </header>
+  <!-- End of header -->
+  <!-- Begining of banner  -->
   <div class="banner">
             <?php include("include/banner.html"); ?>
         </div>
+        <!-- Ending of banner  -->
+        <!-- Begining container -->
   <div class="container">
+    <!-- Begining of wrapper  -->    
   <div id="wrapper">
+  <!-- Begining of section -->
     <section>
+      <!-- Beginign of article  -->
       <article>
-        <br />
+                <!-- Begining of subTitle -->
+        <br />  
         <div class="subTitle">
-          <h2>>_Checkout what we can do for you</h2>
-          <h2>>_Need a website or mobile app contact us</h2>
+          <h2>>_Checkout what we can do for you</h2>          
         </div>
         <br />
+        <!-- Ending of subleTitle  -->
         <!-- Bootstrap website cards -->
-        <div class="card-deck">
-          <div class="card">
-            <img class="card-img-top" src="pictures/headphones.png" alt="Card image cap">
+        <!-- Begining of card-deck  -->
+        <div class="card-deck"> 
+          <!-- Begining of Ecommerce -->
+          <div class="card"> 
+            <img class="card-img-top" src="pictures/headphones.png" alt="Card image cap">      
             <div class="card-body">
-              <h5 class="card-title">Ecommerce:</h5>
+            <h5 class="card-title">Ecommerce:</h5>            
               <p class="card-text"> make your ecommerce website look fantastic</p>
-            </div>
-            <div class="card-footer">
-              <small class="text-muted">Last updated 3 mins ago</small>
-            </div>
+            </div> 
           </div>
+          <!-- Begining of One page site -->
           <div class="card">
             <img class="card-img-top" src="pictures/bestcity.png" alt="Card image cap">
             <div class="card-body">
-              <h5 class="card-title">One,Two, Three Website:</h5>
+              <h5 class="card-title">One page site</h5>
               <p class="card-text">Need something as one two three; we can help you</p>
             </div>
-            <div class="card-footer">
-              <small class="text-muted">Last updated 3 mins ago</small>
-            </div>
           </div>
+          <!-- Begining of Blog -->
           <div class="card">
             <img class="card-img-top" src="pictures/cyclepathology.png" alt="Card image cap">
             <div class="card-body">
               <h5 class="card-title">Blog:</h5>
               <p class="card-text">Need to express your passion then let us help you.</p>
             </div>
-            <div class="button">
-              <small class="text-muted">Last updated 3 mins ago</small>
-            </div>
           </div>
+
         </div>
+        <!-- Ending of card-deck  -->
         <!-- Adds break line -->
         <br />
+        <!-- Begining of card-deck  -->
         <div class="card-deck">
+          <!-- Begining of Full Stack  -->
           <div class="card">
             <img class="card-img-top" src="pictures/check.png" alt="Card image cap">
             <div class="card-body">
               <h5 class="card-title">Full Stack:</h5>
               <p class="card-text">Need a full stack web application, we can make it happen</p>
             </div>
-            <div class="card-footer">
-              <small class="text-muted">Last updated 3 mins ago</small>
-            </div>
           </div>
+          <!-- Begining of Landing page  -->
           <div class="card">
             <img class="card-img-top" src="pictures/LakeTahoe.png" alt="Card image cap">
             <div class="card-body">
               <h5 class="card-title">Landing page:</h5>
               <p class="card-text">Need a landing page; we can help you</p>
             </div>
-            <div class="card-footer">
-              <small class="text-muted">Last updated 3 mins ago</small>
-            </div>
           </div>
+          
           <div class="card">
             <img class="card-img-top" src="pictures/Profeesional.png" alt="Card image cap">
             <div class="card-body">
               <h5 class="card-title">Blog:</h5>
               <p class="card-text">Need to express your passion then let us help you.</p>
             </div>
-            <div class="button">
-              <small class="text-muted">Last updated 3 mins ago</small></div>
+          </div>
+        </div>
+        <div class="card">
+            <img class="card-img-top" src="pictures/HostingService.png " alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">Hosting/Service</h5>
+              <p class="card-text">Reliable website hosting, email, and affordabe domain prices. </p>
+            </div>           
+            <a href="https://www.secureserver.net/?prog_id=566807" class="btn btn-primary">Get your domain</a>      
           </div>
         </div>
       </article>
     </section>
   </div>
-</div>
+</div><!-- class container -->
   <footer class="main-footer">
     <?php include("include/footer.html"); ?>
   </footer>
