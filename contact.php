b34c612168900d90ce64b70be792de11e003eeea
mlandy09 <mc_louigene@yahoo.com>
2020-08-21 19:39:20 -0400

    Add javascript

--- contact.php
+++ contact.php
@@ -30,9 +30,8 @@
                             <!--Section heading-->
                             <h2 class="h1-responsive font-weight-bold text-center my-4">Contact us</h2>
                             <!--Section description-->
-                            <p class="text-center w-responsive mx-auto mb-5">Do you have any questions? Need a website or web application, Please do not hesitate to contact us directly. Our team will come back to you within
-                                a matter of hours to help you.
-                            </p>
+                            
+                            
                             <div class="row">
                                 <!--Grid column-->
                                 <div class="col-md-9 mb-md-0 mb-5">
